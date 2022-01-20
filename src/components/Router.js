354e12943465382de2326743f580901af559afed
do-bright <godqhrgotjs@gmail.com>
2022-01-20 23:14:25 +0900

    로그아웃 (Profile.js 에서 로그아웃 버튼을 통해)

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,24 +1,39 @@
 import React, { useState } from "react";
-import { HashRouter as Router, Route, Siwtch, Switch } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Siwtch,
+  Switch,
+} from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
   //const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
+    // && ; Navigation이 존재하려면 isLoggedIn이 true 여야 함 = True 일 때 이게 존재
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home />
             </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
           </>
         ) : (
+          // path가 / 에 있으면 Auth 에 가고, 그렇지 않으면 from="*" to="/"
           <>
             <Route exact path="/">
               <Auth />
             </Route>
+            <Redirect from="*" to="/" />
           </>
         )}
       </Switch>
